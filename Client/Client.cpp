419f28627b34b9ba6b23386e8f1da3acb0b2d898
Ashton Stockenberg <as.j.stockenberg@Baypath.net>
2024-03-21 14:18:31 -0400

    Client hotfix

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,12 +1,14 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <iostream>
+#include <fstream>
 
 #include "httplib/httplib.h"
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
 
 httplib::Client cli("localhost", 3000);
+std::fstream file;
 
 struct User {
     std::string username;
@@ -20,6 +22,13 @@ std::string GetHWID()
     if (!GetCurrentHwProfileA(&info)) return "";
     
     return info.szHwProfileGuid;
+}
+
+void GetSavedData()
+{
+    std::stringstream ss;
+    ss << file.rdbuf();
+    std::cout << ss.str();
 }
 
 bool CreateAccount()
@@ -92,6 +101,14 @@ bool Login()
 
 int main()
 {
+    char* temp;
+    size_t size;
+    _dupenv_s(&temp, &size, "appdata");
+    std::string path = std::string(temp) + "\\auth_data.txt";
+    file.open(path, std::fstream::out | std::fstream::app | std::fstream::in);
+    
+    GetSavedData();
+
     if (auto res = cli.Get("/"))
     {
         SetConsoleTitleA("Connection Established");
@@ -119,4 +136,6 @@ int main()
             if (CreateAccount()) break;
         }
     }
+
+    file.close();
 }
